ae16b4f02ee3eada924620d31a5b6439dda151ae
HwangSoonHo <hsh5933@naver.com>
2022-10-18 17:07:53 +0900

    init

---
+++ src/test/java/com/hshgroup/myhome/MyhomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hshgroup.myhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
